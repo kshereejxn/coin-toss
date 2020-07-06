edf3300e164f81fafba581a6676cb1170a1c0637
Trinie J <jxsn00ks@gmail.com>
2020-07-06 15:25:07 -0400

    7-6-20 status

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+let bodyElement = document.querySelector ('body')
+
 let coin = {
     state: 0,
     flip: function() {
@@ -13,7 +15,6 @@ let coin = {
            "this.state" is 0 or 1. */
            coin.flip ()
            if (this.state === 0){
-               
                console.log('heads')
            }
            else {
@@ -22,11 +23,30 @@ let coin = {
     },
     toHTML: function() {
         let image = document.createElement('img');
+        
         //call the flip function inside here as well
         //if/else statement set to an image, use append
         /* 3. Set the properties of this image element to show either face-up
            or face-down, depending on whether this.state is 0 or 1.*/
+
+           if (this.state === 0) {
+            image = "heads"
+            image.setAttribute("src", "images/pennyhead.png");
+            image.setAttribute("width", "250");
+            image.setAttribute("height", "250");
+            image.setAttribute("alt", "heads");
+            bodyElement.append(image)
+           }
         return image;
     }
+    //else stmt
+     
 };
+
+function check(){
+    coin.toString()
+}
+check()
 //write 2 functions that roll the coin 20x
+
+
