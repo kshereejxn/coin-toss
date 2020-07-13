cedf9f559cca1a97a867102ae28a500b9e741aac
Trinie J <jxsn00ks@gmail.com>
2020-07-12 23:19:38 -0400

    updated 7-12-20

--- main.js
+++ main.js
@@ -3,45 +3,61 @@ let bodyElement = document.querySelector ('body')
 let coin = {
     state: 0,
     flip: function() {
-        //use math.random and set it to "this.state"
         this.state = Math.floor(Math.random() * 2)
+        console.log (this.state)
+
+        //use math.random and set it to "this.state"
         /* 1. Randomly set your coin object's "state" property to be either 
            0 or 1: use "this.state" to access the "state" property on this object.*/
     },
     toString: function() {
+        coin.flip ()
+           if (this.state === 0){ 
+            document.body.append('Heads')
+            console.log('heads')
+
+
+           }
+           else {
+            document.body.append('Tails')
+             console.log('tails')
+           } 
+           
         //use if/else statement depending on the flip and write it on the HTML, use append
         //call the flip function inside here
         /* 2. Return the string "Heads" or "Tails", depending on whether
            "this.state" is 0 or 1. */
-           coin.flip ()
-           if (this.state === 0){
-               console.log('heads')
-           }
-           else {
-               console.log('tails')
-           }
     },
     toHTML: function() {
-        let image = document.createElement('img');
+        coin.flip ()
+           if (this.state === 0){ 
+        
+        let headsImg = document.createElement('img')
+        headsImg.src = 'https://www.marshu.com/articles/images-website/articles/presidents-on-coins/penny-cent-coin-head.jpg'
+        headsImg.className = 'img'
+        bodyElement.append(headsImg)
+        return headsImg
+            
+           } 
+           else {
+        let tailsImg = document.createElement('img')
+        tailsImg.src = 'http://clipart-library.com/img1/985331.jpg'
+        tailsImg.className = 'img'
+        bodyElement.append(tailsImg) 
+        return tailsImg
+
+           } 
+
+        
         
         //call the flip function inside here as well
         //if/else statement set to an image, use append
         /* 3. Set the properties of this image element to show either face-up
            or face-down, depending on whether this.state is 0 or 1.*/
 
-           if (this.state === 0) {
-            image = "heads"
-            image.setAttribute("src", "images/pennyhead.png");
-            image.setAttribute("width", "250");
-            image.setAttribute("height", "250");
-            image.setAttribute("alt", "heads");
-            bodyElement.append(image)
-           }
-        return image;
     }
-    //else stmt
      
-};
+}
 
 function check(){
     coin.toString()
